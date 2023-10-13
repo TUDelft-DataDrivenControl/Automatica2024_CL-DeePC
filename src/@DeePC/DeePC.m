62acd8378f79b91dc38173a570420a830cd45a05
rtodinkla <r.t.o.dinkla@tudelft.nl>
2023-10-13 13:46:57 +0200

    add DeePC.m as subclass of Generalized DeePC

---
+++ src/@DeePC/DeePC.m
@@ -0,0 +1,29 @@
+classdef DeePC < Generalized_DeePC
+    %CL_DEEPC Summary of this class goes here
+    %   Detailed explanation goes here
+    methods
+        function obj = DeePC(u,y,p,f,N,Q,R,dR,options,con_user,solve_type)
+            arguments
+                u (:,:) double
+                y (:,:) double
+                p (1,1) double {mustBeInteger,mustBePositive}
+                f (1,1) double {mustBeInteger,mustBePositive}
+                N (1,1) double {mustBeInteger,mustBePositive}
+                Q (:,:) double
+                R (:,:) double
+                dR (:,:) double
+                options.use_IV logical   = true
+                options.adaptive logical = true
+                con_user.constr struct = struct('expr',[],'u0_sdp',[],'uf_sdp',[],'y0_sdp',[],'yf_sdp',[]);
+                solve_type.UseOptimizer logical = true
+                solve_type.sdp_opts struct = sdpsettings('solver','mosek','verbose',0);
+            end
+            fid = f;
+            options = namedargs2cell(options);
+            con_user= namedargs2cell(con_user);
+            solve_type = namedargs2cell(solve_type);
+            obj = obj@Generalized_DeePC(u,y,p,f,fid,N,Q,R,dR,options{:}, con_user{:}, solve_type{:});
+        end
+    end
+end
+
