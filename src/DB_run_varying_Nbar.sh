845cf0b2a763227d56f3b81960155849043aa893
rtodinkla <r.t.o.dinkla@tudelft.nl>
2023-11-27 12:56:07 +0100

    increase CPU memory to 4GB

--- src/DB_run_varying_Nbar.sh
+++ src/DB_run_varying_Nbar.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=41
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --account=research-3me-dcsc
 #SBATCH --output=d_Nbar.%j.out
 #SBATCH --error=d_Nbar.%j.err
