f332be289c58c1f6b6514907f530c1c63d131a18
rtodinkla <r.t.o.dinkla@tudelft.nl>
2023-12-05 20:36:22 +0100

    add processing files

--- src/d_Re/d_Re_processing.m
+++ src/d_Re/d_Re_processing.m
@@ -5,88 +5,82 @@ run_dir = pwd;
 contents = dir(run_dir);
 
 % Extract folder names
-folderNames = {contents([contents.isdir]).name};
+fileNames = {contents(~[contents.isdir]).name};
 
 % Define the regular expression pattern
-pattern = '^Re_(\d+(\.\d+)?)$';
+pattern = '^Re_(.*).mat$';
 
 % Apply regexp to the entire cell array
-matches = regexp(folderNames, pattern, 'tokens', 'once');
+matches = regexp(fileNames, pattern, 'tokens', 'once');
 matches = matches(~cellfun('isempty', matches)); % non-empty matches only
+Re_all = cellfun(@(x) str2double(x{1}),matches);
+Re_all = sort(Re_all,2,'ascend');
+
+% get Nbar
+Nbar = regexp(run_dir,'.*_Nbar_(\d+)_.*','tokens','once');
+Nbar = str2double(Nbar{1});
+% get p
+p = regexp(run_dir,'.*_p_(\d+)_.*','tokens','once');
+p = str2double(p{1});
+% get f
+f = regexp(run_dir,'.*_f_(\d+)_.*','tokens','once');
+f = str2double(f{1});
+% get Ru
+Ru = regexp(run_dir,'.*_Ru_(\d+(\.\d+)?)_.*','tokens','once');
+Ru = str2double(Ru{1});
+% get dRu
+Rdu = regexp(run_dir,'.*_Rdu_(\d+(\.\d+)?)_.*','tokens','once');
+Rdu = str2double(Rdu{1});
+% get Q
+Q = regexp(run_dir,'.*_Q_(\d+(\.\d+)?)_.*','tokens','once');
+Q = str2double(Q{1});
+% get dR
+dR = regexp(run_dir,'.*_dR_(\d+(\.\d+)?).*','tokens','once');
+dR = str2double(dR{1});
 
 %%
 num_n = numel(matches);
-% results = repmat(struct('e',[],'u_OL',[],'du_CL',[],'y_OL',[],'x_OL',[],'u_CL',[],...
-%     'y_CL',[],'x_CL',[],'Cost',[],'eLu',[],'eLy',[],'eGu',[],'eObX',[]),1,num_n);
 
-% iterate over folders / noise variances Re
-for kn = 1:num_n
-    % data files have structure kn_%d_ke_%d_ks_%d.mat
-    % -> find folder & files for specific kn (-> Re)
-    [dir_Re,mat_fns,ke_all,ks_all,Re] = kn2dir(kn);
-
-    % Initialize temporary structure array for this iteration
-    tempData = struct();
-
-    % iterate over files / noise realizations using parfor
-    num_e = length(ke_all);
-    tic
-    parfor ce = 1:num_e
-        ke = ke_all(ce);
-        ks = ks_all(ce);
-        mat_fn = mat_fns{ce};
-
-        % load data
-        s = load(fullfile(dir_Re, mat_fn));
-        fns = fieldnames(s);
-        for k_fld = 1:length(fns)
-            fn = fns(k_fld); fn = fn{1};
-            tempData(ce).(fn) = s.(fn);
-        end
-    end
-    toc
-
-    % Concatenate the temporary structure array to the main structure array
-    results(kn).Re = Re;
-    fns = fieldnames(tempData);
-    for k_fld = 1:length(fns)
-        fn = fns(k_fld); fn = fn{1};
-        results(kn).(fn) = tempData.(fn);
-    end
+% iterate over noise variances Re
+quantiles = 0.1:0.1:0.9;%[0.1 0.25 0.5 0.75 0.9];
+n_quant = length(quantiles); idx_median = ceil(n_quant/2);
+costs_DeePC1 = nan(n_quant,num_n); % quantiles w/ median of cost
+costs_CLDeePC1 = costs_DeePC1;
+costs_DeePC2 = nan(1,num_n);       % mean cost
+costs_CLDeePC2 = costs_DeePC2;
+parfor kn = 1:num_n
+    Re = Re_all(kn);
+    fn = strcat('Re_',num2str(Re),'.mat');
+    
+    % load results of all noise realizations for specific Re
+    a = load(fn,'results');
+    costs = cellfun(@(x) mean(x(Nbar+1:end)),a.results.Cost);
+    costs_DeePC1(:,kn)   = quantile(costs(:,1),quantiles);
+    costs_CLDeePC1(:,kn) = quantile(costs(:,2),quantiles);
+    costs_DeePC2(:,kn)   = mean(costs(:,1));
+    costs_CLDeePC2(:,kn) = mean(costs(:,2));
 end
 %%
-function [folder,matFiles,ke,ks,Re] = kn2dir(kn)
-    % Specify the path to the directory
-    parentDirectory = pwd;
-    
-    % Define the regular expression pattern
-    pattern = strcat('^kn_',num2str(kn),'_ke_(\d+)_ks_(\d+)\.mat');
-    
-    % Get a list of all .mat files in the directory
-    matFiles = dir(fullfile(parentDirectory, '**/*.mat'));
-    
-    % Extract file names
-    fileNames = {matFiles.name};
-    
-    % Apply regexp to the entire cell array
-    matches = regexp(fileNames, pattern, 'tokens', 'once');
-    
-    % Find the indices of the non-empty matches
-    indices = find(~cellfun('isempty', matches));
-    
-    % get folder
-    folder = matFiles(indices(1)).folder;
-    Re = regexp(folder,'Re_(\d+\.?\d*)$','tokens','once');
-    Re = str2double(Re{1});
-
-    % select all relevant .mat files
-    matFiles = {matFiles(indices).name};
-
-
-    ke = cellfun(@(x) str2double(x{1}),matches(indices));%,'UniformOutput',false);
-    ks = cellfun(@(x) str2double(x{2}),matches(indices));%,'UniformOutput',false);
+color = {[220,50,32]/256,[0,90,181]/256};%{'#DC3220','#005AB5'};
+figure();
+l1 = semilogy(Re_all,costs_DeePC1(idx_median,:),   Color=color{1},LineWidth=2.5); hold on;
+l11= semilogy(Re_all,costs_DeePC2,Color=color{1},  LineStyle='--',LineWidth=1.5);
+l2 = semilogy(Re_all,costs_CLDeePC1(idx_median,:), Color=color{2},LineWidth=2.5);
+l21= semilogy(Re_all,costs_CLDeePC2,Color=color{2},LineStyle='--',LineWidth=1.5);
+leg = legend('DeePC (median)','DeePC (mean)','CL-DeePC (median)','CL-DeePC (mean)');
+set(leg,'Interpreter','latex');
+ff1 = cell(1,idx_median-1);
+ff2 = ff1;
+FaceAlpha = 0.6/(idx_median-1);
+for idx = 1:idx_median-1
+    ff1{idx}=fill([Re_all fliplr(Re_all)],[costs_DeePC1(idx,:),  fliplr(costs_DeePC1(end-idx+1,:))],  Color{1},HandleVisibility='off');
+    ff1{idx}.EdgeAlpha = 0.1; ff1{idx}.FaceAlpha = FaceAlpha; hold on;
+    ff2{idx}=fill([Re_all fliplr(Re_all)],[costs_CLDeePC1(idx,:),fliplr(costs_CLDeePC1(end-idx+1,:))],Color{2},HandleVisibility='off');
+    ff2{idx}.EdgeAlpha = 0.1; ff2{idx}.FaceAlpha = FaceAlpha; hold on;
 end
-
-% function [Re,Cost] = get_Re_Cost(kn,ke)
-%     
-% end
+grid on
+xlabel('$\sigma^2(e_k)$','Interpreter','latex');
+ylabel('$\bar{J}$','Interpreter','latex');
+title(append('$\bar{N}=',num2str(Nbar),'$, ',...
+      '$\sigma^2(d^\mathrm{u}_k)=',num2str(Rdu),'$, ',...
+      '$p=',num2str(p),'$, $f=',num2str(f),'$'),'Interpreter','latex');
--- src/d_pf/d_pf_processing.m
+++ src/d_pf/d_pf_processing.m
@@ -8,24 +8,20 @@ contents = dir(run_dir);
 fileNames = {contents(~[contents.isdir]).name};
 
 % Define the regular expression pattern
-pattern = '^Nbar_(.*).mat$';
+pattern = '^p_(\d+)_f_\d+.mat$';
 
 % Apply regexp to the entire cell array
 matches = regexp(fileNames, pattern, 'tokens', 'once');
 matches = matches(~cellfun('isempty', matches)); % non-empty matches only
-Nbar_all = cellfun(@(x) str2double(x{1}),matches);
-Nbar_all = sort(Nbar_all,2,'ascend');
-Nbar_max = Nbar_all(end);
+pf_all = cellfun(@(x) str2double(x{1}),matches);
+pf_all = sort(pf_all,2,'ascend');
 
+% get Nbar
+Nbar = regexp(run_dir,'.*_Nbar_(\d+)_.*','tokens','once');
+Nbar = str2double(Nbar{1});
 % get Re
 Re = regexp(run_dir,'.*_Re_(\d+(\.\d+)?)_.*','tokens','once');
 Re = str2double(Re{1});
-% get p
-p = regexp(run_dir,'.*_p_(\d+)_.*','tokens','once');
-p = str2double(p{1});
-% get f
-f = regexp(run_dir,'.*_f_(\d+)_.*','tokens','once');
-f = str2double(f{1});
 % get Ru
 Ru = regexp(run_dir,'.*_Ru_(\d+(\.\d+)?)_.*','tokens','once');
 Ru = str2double(Ru{1});
@@ -44,18 +40,18 @@ num_n = numel(matches);
 
 % iterate over noise variances Re
 quantiles = 0.1:0.1:0.9;%[0.1 0.25 0.5 0.75 0.9];
-n_quant = length(quantiles); idx_mean = ceil(n_quant/2);
+n_quant = length(quantiles); idx_median = ceil(n_quant/2);
 costs_DeePC1 = nan(n_quant,num_n); % quantiles w/ median of cost
 costs_CLDeePC1 = costs_DeePC1;
 costs_DeePC2 = nan(1,num_n);       % mean cost
 costs_CLDeePC2 = costs_DeePC2;
 parfor kn = 1:num_n
-    Nbar = Nbar_all(kn);
-    fn = strcat('Nbar_',num2str(Nbar),'.mat');
+    pf = pf_all(kn);
+    fn = strcat('p_',num2str(pf),'_f_',num2str(pf),'.mat');
     
     % load results of all noise realizations for specific Re
     a = load(fn,'results');
-    costs = cellfun(@(x) mean(x(Nbar+1:end)),a.results.Cost); % Nbar changes -> diff. #samples for mean! account for larger variance of mean?
+    costs = cellfun(@(x) mean(x(Nbar+1:end)),a.results.Cost);
     costs_DeePC1(:,kn)   = quantile(costs(:,1),quantiles);
     costs_CLDeePC1(:,kn) = quantile(costs(:,2),quantiles);
     costs_DeePC2(:,kn)   = mean(costs(:,1));
@@ -64,25 +60,24 @@ end
 %%
 color = {[220,50,32]/256,[0,90,181]/256};%{'#DC3220','#005AB5'};
 figure();
-l1 = semilogy(Nbar_all,costs_DeePC1(idx_median,:),   Color=color{1},LineWidth=2.5); hold on;
-l11= semilogy(Nbar_all,costs_DeePC2,Color=color{1},  LineStyle='--',LineWidth=1.5);
-l2 = semilogy(Nbar_all,costs_CLDeePC1(idx_median,:), Color=color{2},LineWidth=2.5);
-l21= semilogy(Nbar_all,costs_CLDeePC2,Color=color{2},LineStyle='--',LineWidth=1.5);
+l1 = semilogy(pf_all,costs_DeePC1(idx_median,:),   Color=color{1},LineWidth=2.5); hold on;
+l11= semilogy(pf_all,costs_DeePC2,Color=color{1},  LineStyle='--',LineWidth=1.5);
+l2 = semilogy(pf_all,costs_CLDeePC1(idx_median,:), Color=color{2},LineWidth=2.5);
+l21= semilogy(pf_all,costs_CLDeePC2,Color=color{2},LineStyle='--',LineWidth=1.5);
 leg = legend('DeePC (median)','DeePC (mean)','CL-DeePC (median)','CL-DeePC (mean)');
 set(leg,'Interpreter','latex');
-ff1 = cell(1,idx_mean-1);
+ff1 = cell(1,idx_median-1);
 ff2 = ff1;
-FaceAlpha = 0.6/(idx_mean-1);
+FaceAlpha = 0.6/(idx_median-1);
 for idx = 1:idx_median-1
-    ff1{idx}=fill([Nbar_all fliplr(Nbar_all)],[costs_DeePC1(idx,:),  fliplr(costs_DeePC1(end-idx+1,:))],  Color{1},HandleVisibility='off');
+    ff1{idx}=fill([pf_all fliplr(pf_all)],[costs_DeePC1(idx,:),  fliplr(costs_DeePC1(end-idx+1,:))],  Color{1},HandleVisibility='off');
     ff1{idx}.EdgeAlpha = 0.1; ff1{idx}.FaceAlpha = FaceAlpha; hold on;
-    ff2{idx}=fill([Nbar_all fliplr(Nbar_all)],[costs_CLDeePC1(idx,:),fliplr(costs_CLDeePC1(end-idx+1,:))],Color{2},HandleVisibility='off');
+    ff2{idx}=fill([pf_all fliplr(pf_all)],[costs_CLDeePC1(idx,:),fliplr(costs_CLDeePC1(end-idx+1,:))],Color{2},HandleVisibility='off');
     ff2{idx}.EdgeAlpha = 0.1; ff2{idx}.FaceAlpha = FaceAlpha; hold on;
 end
 grid on
-xlim([Nbar_all(1) Nbar_all(end)])
-xlabel('$\bar{N}$','Interpreter','latex');
+xlabel('$f(=p)$','Interpreter','latex');
 ylabel('$\bar{J}$','Interpreter','latex');
-title(append('$\sigma^2(e_k)=',num2str(Re),'$, ',...
-      '$\sigma^2(d^\mathrm{u}_k)=',num2str(Rdu),'$, ',...
-      '$p=',num2str(p),'$, $f=',num2str(f),'$'),'Interpreter','latex');
+title(append('$\bar{N}=',num2str(Nbar),'$, ',...
+      '$\sigma^2(e_k)=',num2str(Re),'$, ',...
+      '$\sigma^2(d^\mathrm{u}_k)=',num2str(Rdu),'$'),'Interpreter','latex');
